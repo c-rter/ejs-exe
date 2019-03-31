721d3a570bae5991492d05e56166a3e110f3bd8b
c-rter <c@rterlawson.com>
2019-03-31 15:48:18 -0400

    recursive function crawl

---
+++ 3 - Recursive Function Test/logic.js
@@ -0,0 +1,18 @@
+const isEven = (target) => {
+  if (target == 0) {
+    return true;
+  }
+  else if (target == 1) {
+    return false;
+  }
+  else {
+    target -= 2;
+    return isEven(target);
+  }
+}
+
+const receiveInput = () => {
+  let gridSize = document.getElementById("targetText").value;
+  document.getElementById("boardSpan").innerHTML = isEven(gridSize);
+
+};
