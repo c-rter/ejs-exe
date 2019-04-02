814ad8f2722c53dfd3cc29f217940b00db399723
Carter Lawson <c@rterlawson.com>
2019-04-02 17:04:51 -0400

    range functionality

--- 4 - Range Creator and Summator/logic.js
+++ 4 - Range Creator and Summator/logic.js
@@ -2,26 +2,35 @@
 
 const createRange = (start, end, step) => {
 
-  rangeArr = []
-  if (step == undefined) {
-    for (i = start; i <= end; i++) {
+  rangeArr = [];
+  let startCount = parseInt(start);
+  let endCount = parseInt(end);
+  let stepCount = parseInt(step);
+
+  if (isNaN(stepCount)) {
+    for (i = startCount; i <= endCount; i++) {
       rangeArr.push(i);
     }
   }
-  else if (step < 0) {
-    for (i = start; i >= end; i -= step) {
+  else if (stepCount < 0) {
+    for (i = startCount; i >= endCount; i -= step) {
       rangeArr.push(i);
     }
   }
   else {
-    for (i = start; i <= end; i += step) {
+    for (i = startCount; i <= endCount; i += stepCount) {
       rangeArr.push(i);
     }
   }
+
+return rangeArr;
+
 }
 
 const receiveInput = () => {
-  let gridSize = document.getElementById("targetText").value;
-  document.getElementById("boardSpan").innerHTML = createRange(gridSize);
+  let startInt = document.getElementById("startInt").value;
+  let endInt = document.getElementById("endInt").value;
+  let stepInt = document.getElementById("stepInt").value;
+  document.getElementById("range").innerHTML = createRange(startInt, endInt, stepInt);
 
 };
