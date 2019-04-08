bc1ad9e5f55ab8e2a035a020b65222ffdf9e1fde
c-rter <c@rterlawson.com>
2019-04-07 20:35:46 -0400

    parsing array in string

--- 5 - Array Reverser/logic.js
+++ 5 - Array Reverser/logic.js
@@ -1,12 +1,14 @@
-const reverse1 = (start, end, step) => {
-  rangeArr = [];
-  return rangeArr;
+const reverse1 = () => {
+  let arrayRaw = document.getElementById("textArray").value;
+  console.log(arrayRaw);
+  console.log(typeof(arrayRaw));
+
 }
 
-const reverse2() = () => {
+const reverse2 = () => {
   currentArray = createRange(startInt, endInt, stepInt);
 }
 
-let stepInt = document.getElementById("stepInt").value;
-document.getElementById("range").innerHTML = currentArray;
+// let stepInt = document.getElementById("stepInt").value;
+// document.getElementById("range").innerHTML = currentArray;
 
