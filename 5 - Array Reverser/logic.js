ad1cca452880b6d4b0b4b11352375bcbec1de903
Carter Lawson <c@rterlawson.com>
2019-04-09 11:17:14 -0400

    work on method2

--- 5 - Array Reverser/logic.js
+++ 5 - Array Reverser/logic.js
@@ -1,19 +1,31 @@
 const reverse1 = () => {
   let arrayRaw = document.getElementById("textArray").value;
-  console.log(arrayRaw);
-  console.log(typeof(arrayRaw));
-  parsedArray = JSON.parse(arrayRaw);
+  let parsedArray = JSON.parse(arrayRaw);
   console.log(parsedArray);
+
   reversedArray = [];
-  for (i=0; i<parsedArray.length; i++) {
-      reversedArray.unshift(parsedArray[i]);
+  for (i = 0; i < parsedArray.length; i++) {
+    reversedArray.unshift(parsedArray[i]);
   }
   document.getElementById("arrayRenderSpot").innerHTML = reversedArray;
 
 }
 
 const reverse2 = () => {
-  currentArray = createRange(startInt, endInt, stepInt);
+  let arrayRaw2 = document.getElementById("textArray").value;
+  let parsedArray2 = JSON.parse(arrayRaw2);
+  console.log(parsedArray2);
+
+  for (i = 0; i < parsedArray2.length; i++) {
+    let tmp;
+
+    parsedArray2[i] = tmp;
+    parsedArray2[i] = parsedArray2[parsedArray2.length - i];
+    parsedArray2[parsedArray2.length - i] = tmp;
+
+  }
+  console.log(parsedArray2);
+
 }
 
 const clearArraySpot = () => {
