193e16d0bca7a67f703915cea87770099bc0687c
Carter <38448166+c-rter@users.noreply.github.com>
2019-04-13 23:00:05 -0400

    cleaning

--- 4 - Array & List Conversions, Combinations, and Searches/logic.js
+++ 4 - Array & List Conversions, Combinations, and Searches/logic.js
@@ -2,10 +2,6 @@ const reverse1 = () => {
   let arrayRaw = document.getElementById("textArray").value;
   let parsedArray = JSON.parse(arrayRaw);
 
-  reversedArray = [];
-  for (i = 0; i < parsedArray.length; i++) {
-    reversedArray.unshift(parsedArray[i]);
-  }
   document.getElementById("arrayRenderSpot").innerHTML = reversedArray;
 
 }
@@ -13,12 +9,7 @@ const reverse1 = () => {
 const reverse2 = () => {
   let arrayRaw2 = document.getElementById("textArray").value;
   let parsedArray2 = JSON.parse(arrayRaw2);
-  
-  for (i = 0; i < (parsedArray2.length/2); i++) {
-    let tmp = parsedArray2[i];
-    parsedArray2[i] = parsedArray2[((parsedArray2.length - i) - 1)];
-    parsedArray2[parsedArray2.length - i - 1] = tmp;
-  }
+
   document.getElementById("arrayRenderSpot").innerHTML = parsedArray2;
 }
 
