12525dd0fc5b2acfb9a0cf829952fc02e22672ff
c-rter <c@rterlawson.com>
2019-04-15 21:20:39 -0400

    build router skeleton

--- 4 - Array & List Conversions, Combinations, and Searches/logic.js
+++ 4 - Array & List Conversions, Combinations, and Searches/logic.js
@@ -1,14 +1,49 @@
 const inputRouter = () => {
 
-  if (1) {
+  let fields = ["arrayToList", "listToArray", "listPrepend",
+    "elementPrepend", "listReturn", "elementReturn"];
 
+  let valiCounter = 0;
+  let routerLog = "";
+
+  for (i = 0; i < fields.length - 1; i++) {
+    if (!(document.getElementById(fields[i]).value == "")) {
+      valiCounter++;
+      routerLog = fields[i];
+    }
   }
 
+  if (valiCounter < 1) {
+    alert("no input!");
+  }
+  else if (valiCounter > 1) {
+    alert("too many inputs!");
+  }
+  else {
+    receiver[routerLog]();
+  }
 }
 
 const clearArraySpot = () => {
   document.getElementById("arrayRenderSpot").innerHTML = "";
 }
+
+let receiver = {
+  arrayToList: function () {
+
+  },
+  listToArray: function () {
+
+  },
+  listPrepend: function () {
+
+  },
+  listReturn: function () {
+
+  }
+
+};
+
 
 /*   let arrayRaw = document.getElementById("textArray").value;
   let parsedArray = JSON.parse(arrayRaw);
