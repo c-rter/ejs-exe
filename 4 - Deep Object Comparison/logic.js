d4e87d70cf8d97e0ca3c903e0946b48dc0778c83
Carter Lawson <c@rterlawson.com>
2019-04-24 14:40:20 -0400

    adapt skeleton to accomodate recursive checks of non objects

--- 4 - Deep Object Comparison/logic.js
+++ 4 - Deep Object Comparison/logic.js
@@ -1,4 +1,18 @@
-const objectAnalyzer = () => {
+const inputHandler = () => {
+
+  let input1 = document.getElementById("input1").value;
+  let input2 = document.getElementById("input2").value;
+
+}
+
+const deepAnalyze = (object1, object2) => {
+  
+//  check typeof
+//  if not object, use built in comparison, return boolean
+//  if objects:
+//  let parsedObject1 = JSON.parse(input1);
+//  let parsedObject2 = JSON.parse(input2);
+//  object keys, run through and recurse until non-objects can be compared
 
 }
 
