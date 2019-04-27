6ceb4d75d6bb63a36ccf99865d017cf2fbba031d
c-rter <c@rterlawson.com>
2019-04-27 00:19:18 -0400

    organize and segment object testing function

--- 4 - Deep Object Comparison/logic.js
+++ 4 - Deep Object Comparison/logic.js
@@ -2,47 +2,41 @@ const inputHandler = () => {
 
   let input1 = document.getElementById("input1").value;
   let input2 = document.getElementById("input2").value;
-  let comparisonStatus = deepAnalyze (input1, input2);
+  let comparisonStatus = deepAnalyze(input1, input2);
+
+}
+
+const testTheObjects = (object1, object2) => {
+
+
 
 }
 
 const deepAnalyze = (input1, input2) => {
-  
-// use try and throw for JSON parse error catching
 
+  try {
+    let parseTest = JSON.parse(input1);
+    let parseTest2 = JSON.parse(input2);
 
-try {
-  console.log(typeof input1);
-  console.log(typeof input2);
-  let parseTest = JSON.parse(input1);
-  let parseTest2 = JSON.parse(input2);
-  console.log(typeof parseTest);
-  console.log(typeof parseTest2);
-
-  if ((typeof parseTest === "object" && parseTest !== null) 
-  && (typeof parseTest2 === "object" && parseTest2 !== null)) {
-  console.log("two objects");
-  
+    if ((typeof parseTest === "object" && parseTest !== null)
+      && (typeof parseTest2 === "object" && parseTest2 !== null)) {
+      console.log("two objects");
+      return testTheObjects(parseTest, parseTest2);
+    }
+    else {
+      console.log("not two objects");
+      if (!(parseTest === parseTest2)) {
+        return false;
+      }
+      else {
+        return true;
+      }
+    }
   }
-  
-  else {
-  console.log("not two objects");
-  
+  catch (err) {
+    alert("not a proper value");
+    return false;
   }
-  
-}
-catch(err) {
-alert("not a proper value");
-} 
-
-
-//  check typeof
-//  if not object, use built in comparison, return boolean
-//  if objects:
-//  let parsedObject1 = JSON.parse(input1);
-//  let parsedObject2 = JSON.parse(input2);
-//  object keys, run through and recurse until non-objects can be compared
-
 }
 
 const clearForm = () => {
