941580a3ba6ba1f7ce08110d897238d7df0ff642
Carter Lawson <c@rterlawson.com>
2019-07-09 13:58:23 -0400

    add completed reduction logic

--- 5 - Array Collapser/logic.js
+++ 5 - Array Collapser/logic.js
@@ -1,4 +1,4 @@
-const collapse = () => {
+/* const collapse = () => {
   let arrayRaw = document.getElementById("textArray").value;
   let parsedArray = JSON.parse(arrayRaw);
 
@@ -10,3 +10,12 @@ const collapse = () => {
 const clearArraySpot = () => {
   document.getElementById("arrayRenderSpot").innerHTML = "";
 }
+
+*/
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+
+// → [1, 2, 3, 4, 5, 6]
+
+let flattenedArray = arrays.reduce((a, b) => a.concat(b));
+console.log(flattenedArray);
