9e5e4598a2a038024204b0b8154b67a72d080609
Carter Lawson <c@rterlawson.com>
2019-07-10 12:07:47 -0400

    add solution logic

--- 5 - Custom Looper/logic.js
+++ 5 - Custom Looper/logic.js
@@ -1,7 +1,12 @@
 let loop = (val, test, update, body) => {
 
+  let currentVal = val;
 
-  
+  while (test(currentVal)) {
+    body(currentVal);
+    currentVal = update(currentVal);
+  }
+
 };
 
 loop(3, n => n > 0, n => n - 1, console.log);
