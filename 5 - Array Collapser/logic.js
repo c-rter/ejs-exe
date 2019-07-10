868a3ba8fd0075cf2c5b8741218d435b5396e74e
Carter Lawson <c@rterlawson.com>
2019-07-10 11:33:15 -0400

    integrate with HTML input, stringify result

--- 5 - Array Collapser/logic.js
+++ 5 - Array Collapser/logic.js
@@ -1,9 +1,11 @@
-/* const collapse = () => {
+ const collapse = () => {
   let arrayRaw = document.getElementById("textArray").value;
   let parsedArray = JSON.parse(arrayRaw);
+  console.log(parsedArray);
+let flattenedArray = parsedArray.reduce((a, b) => a.concat(b));
+console.log(flattenedArray);
 
-
-//  document.getElementById("arrayRenderSpot").innerHTML = parsedArray;
+  document.getElementById("arrayRenderSpot").innerHTML = JSON.stringify(flattenedArray);
 }
 
 
@@ -11,11 +13,13 @@ const clearArraySpot = () => {
   document.getElementById("arrayRenderSpot").innerHTML = "";
 }
 
-*/
 
-let arrays = [[1, 2, 3], [4, 5], [6]];
+
+/* let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // → [1, 2, 3, 4, 5, 6]
 
 let flattenedArray = arrays.reduce((a, b) => a.concat(b));
 console.log(flattenedArray);
+
+*/
