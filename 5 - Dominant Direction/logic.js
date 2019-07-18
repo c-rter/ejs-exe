2149cffaa8045a3dbf4d27b3c19689acd6236a94
Carter Lawson <c@rterlawson.com>
2019-07-18 14:45:24 -0400

    add working prototype, error in single script input

--- 5 - Dominant Direction/logic.js
+++ 5 - Dominant Direction/logic.js
@@ -1,5 +1,5 @@
- 
- var SCRIPTS = [
+
+var SCRIPTS = [
   {
     name: "Adlam",
     ranges: [[125184, 125259], [125264, 125274], [125278, 125280]],
@@ -1113,68 +1113,48 @@
     link: "https://en.wikipedia.org/wiki/Mongolian_writing_systems#Horizontal_square_script"
   }
 ];
- 
- // via ejs c5
 
- function characterScript(code) {
-    for (let script of SCRIPTS) {
-      if (script.ranges.some(([from, to]) => {
-        return code >= from && code < to;
-      })) {
-        return script;
-      }
+// via ejs c5
+
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if (known == -1) {
+      counts.push({ name, count: 1 });
+    } else {
+      counts[known].count++;
     }
-    return null;
   }
-  
-  console.log(characterScript(121));
-  // → {name: "Latin", …}
-
-   // via ejs c5
-
-  function countBy(items, groupName) {
-    let counts = [];
-    for (let item of items) {
-      let name = groupName(item);
-      let known = counts.findIndex(c => c.name == name);
-      if (known == -1) {
-        counts.push({name, count: 1});
-      } else {
-        counts[known].count++;
-      }
-    }
-    return counts;
-  }
-  
-  console.log(countBy([1, 2, 3, 4, 5], n => n > 2));
-  // → [{name: false, count: 2}, {name: true, count: 3}]
-
-  function dominantDirection(text) {
-    // Your code here.
-  }
-  
-  console.log(dominantDirection("Hello!"));
-  // → ltr
-  console.log(dominantDirection("Hey, مساء الخير"));
-  // → rtl
-
-  /* Sample Function, Similar
-
-  function textScripts(text) {
-  let scripts = countBy(text, char => {
-    let script = characterScript(char.codePointAt(0));
-    return script ? script.name : "none";
-  }).filter(({name}) => name != "none");
-
-  let total = scripts.reduce((n, {count}) => n + count, 0);
-  if (total == 0) return "No scripts found";
-
-  return scripts.map(({name, count}) => {
-    return `${Math.round(count * 100 / total)}% ${name}`;
-  }).join(", ");
+  return counts;
 }
 
-console.log(textScripts('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
-// → 61% Han, 22% Latin, 17% Cyrillic 
 
-*/
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (script.ranges.some(([from, to]) => {
+      return code >= from && code < to;
+    })) {
+      return script;
+    }
+  }
+  return null;
+}
+
+
+function dominantDirection(text) {
+  let directionDirectory = countBy(text, char => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({ name }) => name != "none").reduce((a, b) => {
+    return a.count > b.count ? a.name : b.name;
+  });
+
+ return directionDirectory;
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
