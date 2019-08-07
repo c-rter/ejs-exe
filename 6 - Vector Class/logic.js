ea3dd1b0c019cbb72c84bb0065df22d74d35086a
c-rter <c@rterlawson.com>
2019-08-07 00:13:39 -0400

    add completed addition and subtraction methods

--- 6 - Vector Class/logic.js
+++ 6 - Vector Class/logic.js
@@ -7,20 +7,22 @@ class Vec {
     console.log(`Hi ${name}. The x coordinate of the vector is ${this.x}, and the y coordinate is ${this.y}.`);
   }
   plus(additionVec) {
-    console.log(additionVec.x);
-    return additionVec.y;
+    return `Vec{x: ${this.x + additionVec.x}, y: ${this.y + additionVec.y}}`;
+  }
+  minus(subtractionVec) {
+    return `Vec{x: ${this.x - subtractionVec.x}, y: ${this.y - subtractionVec.y}}`;
   }
 }
 
-let vecTest = new Vec(1, 2);
+// let vecTest = new Vec(1, 2);
 // console.log(vecTest.x);
 // console.log(vecTest.y);
 // console.log(vecTest.speak("c-rter"));
-console.log(vecTest.plus(new Vec(5, 10)));
+// console.log(vecTest.plus(new Vec(5, 10)));
 
-// console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+ console.log(new Vec(1, 2).plus(new Vec(2, 3)));
 // → Vec{x: 3, y: 5}
-// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+ console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 // console.log(new Vec(3, 4).length);
 // → 5
