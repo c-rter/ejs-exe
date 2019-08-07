6313c8252cee859dec4ba4bfc4e3a2cb29094b38
c-rter <c@rterlawson.com>
2019-08-06 23:54:32 -0400

    add template and skeleton class

---
+++ 6 - Vector Class/logic.js
@@ -0,0 +1,21 @@
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+  speak(name) {
+    console.log(`Hi ${name}. The x coordinate of the vector is ${this.x}, and the y coordinate is ${this.y}.`);
+  }
+}
+
+let vecTest = new Vec (1, 2);
+console.log(vecTest.x);
+console.log(vecTest.y);
+console.log(vecTest.speak("c-rter"));
+
+// console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// → Vec{x: 3, y: 5}
+// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// → Vec{x: -1, y: -1}
+// console.log(new Vec(3, 4).length);
+// → 5
