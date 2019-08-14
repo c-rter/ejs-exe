145804d9611529e001969e7dcc31ff2660560840
Carter Lawson <c@rterlawson.com>
2019-08-14 11:00:32 -0400

    add add, delete, has methods

--- 6 - Group Value Tracker/logic.js
+++ 6 - Group Value Tracker/logic.js
@@ -1,13 +1,53 @@
 class Group {
-    // Your code here.
-  }
-  
-  let group = Group.from([10, 20]);
-  console.log(group.has(10));
-  // → true
-  console.log(group.has(30));
+
+    constructor() {
+        this.collection = [];
+    }
+
+    add(val) {
+        if (this.collection.indexOf(val) === -1) {
+            this.collection.push(val);
+        }
+        else {
+            console.log("already in collection");
+        }
+    }
+
+    delete(val) {
+        if (this.collection.indexOf(val) === -1) {
+            console.log("not in collection");
+        }
+        else {
+            this.collection.splice(this.collection.indexOf(val), 1);
+        }
+    }
+
+    has(val) {
+        if (this.collection.indexOf(val) === -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+}
+
+let group = new Group;
+group.add(2);
+console.log(group.collection);
+group.delete(2);
+console.log(group.collection);
+/*
+
+let group = Group.from([10, 20]);
+console.log(group.has(10));
+// → true
+console.log(group.has(30));
+// → false
+group.add(10);
+group.delete(10);
+console.log(group.has(10));
   // → false
-  group.add(10);
-  group.delete(10);
-  console.log(group.has(10));
-  // → false
+
+*/
