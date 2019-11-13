097780576f41e865756fb52e4474e1ce3cbd03a2
Carter Lawson <c@rterlawson.com>
2019-11-13 12:12:12 -0500

    placement

--- 7 - Efficient Robot/efficientRobot.js
+++ 7 - Efficient Robot/efficientRobot.js
@@ -149,9 +149,18 @@ function routeRobot(state, memory) {
 
 
 // Robot Type III - Smart Route Calculator ----------
-// Sample { 'Alice\'s House': [ 'Bob\'s House', 'Cabin', 'Post Office' ]}
-// Sample { 'Bob\'s House': [ 'Alice\'s House', 'Town Hall' ], }
-// Place = Alice's House, Target = Town Hall
+
+/* { 'Alice\'s House': [ 'Bob\'s House', 'Cabin', 'Post Office' ],
+  'Bob\'s House': [ 'Alice\'s House', 'Town Hall' ],
+  Cabin: [ 'Alice\'s House' ],
+  'Post Office': [ 'Alice\'s House', 'Marketplace' ],
+  'Town Hall': [ 'Bob\'s House', 'Daria\'s House', 'Marketplace', 'Shop' ],
+  'Daria\'s House': [ 'Ernie\'s House', 'Town Hall' ],
+  'Ernie\'s House': [ 'Daria\'s House', 'Grete\'s House' ],
+  'Grete\'s House': [ 'Ernie\'s House', 'Farm', 'Shop' ],
+  Farm: [ 'Grete\'s House', 'Marketplace' ],
+  Shop: [ 'Grete\'s House', 'Marketplace', 'Town Hall' ],
+  Marketplace: [ 'Farm', 'Post Office', 'Shop', 'Town Hall' ] } */
 
 function findRoute(graph, from, to) {
     console.log(`From: ${from}, To: ${to}`)
