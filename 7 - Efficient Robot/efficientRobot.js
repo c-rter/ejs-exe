19646f6a0ca66dc36199d17e8e2505e666945c36
Carter Lawson <c@rterlawson.com>
2019-11-13 12:10:10 -0500

    helpful log

--- 7 - Efficient Robot/efficientRobot.js
+++ 7 - Efficient Robot/efficientRobot.js
@@ -155,10 +155,13 @@ function routeRobot(state, memory) {
 
 function findRoute(graph, from, to) {
     let work = [{ at: from, route: [] }];
-    for (let i = 0; i < work.length; i++) { 
+    for (let i = 0; i < work.length; i++) {
         let { at, route } = work[i];
         for (let place of graph[at]) {
-            if (place == to) return route.concat(place);
+            if (place == to) {
+                console.log(work);
+                return route.concat(place);
+            }
             if (!work.some(w => w.at == place)) {
                 work.push({ at: place, route: route.concat(place) });
             }
