81b0cdd363e17f596acef3d4c69eed97597343b2
c-rter <c@rterlawson.com>
2020-07-09 12:51:49 -0400

    add ious solution

--- 9 - Regex Golf/c9e1.js
+++ 9 - Regex Golf/c9e1.js
@@ -30,7 +30,7 @@ verify(/ferr(et|y|ari)/,
        ["ferret", "ferry", "ferrari"],
        ["ferrum", "transfer A"]);
 
-verify(/.../,
+verify(/ious\b/,
        ["how delicious", "spacious room"],
        ["ruinous", "consciousness"]);
 
