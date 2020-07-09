d78fe2346d396938e46d96cd2ff14fe056ad8a06
c-rter <c@rterlawson.com>
2020-07-09 12:49:02 -0400

    add description and solution 1-3

--- 9 - Regex Golf/c9e1.js
+++ 9 - Regex Golf/c9e1.js
@@ -1,14 +1,32 @@
 // Fill in the regular expressions
 
+/* 
+
+car and cat
+
+pop and prop
+
+ferret, ferry, and ferrari
+
+Any word ending in ious
+
+A whitespace character followed by a period, comma, colon, or semicolon
+
+A word longer than six letters
+
+A word without the letter e (or E)
+
+*/
+
 verify(/ca(r|t)/,
        ["my car", "bad cats"],
        ["camper", "high art"]);
 
-verify(/.../,
+verify(/pr?op/,
        ["pop culture", "mad props"],
        ["plop", "prrrop"]);
 
-verify(/.../,
+verify(/ferr(et|y|ari)/,
        ["ferret", "ferry", "ferrari"],
        ["ferrum", "transfer A"]);
 
