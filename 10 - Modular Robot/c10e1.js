2045dbef820ed4216d271e9e8b60660e8941fbea
c-rter <c@rterlawson.com>
2020-07-11 21:18:01 -0400

    add modular segmentation ideas

--- 10 - Modular Robot/c10e1.js
+++ 10 - Modular Robot/c10e1.js
@@ -14,3 +14,15 @@ goalOrientedRobot
 
 */
 
+/*
+
+buildGraph could be created as a module, which would receive roads format (or more) and return the unique data structure in roadGraph.
+
+Randomized VillageState objects could be returned from a module.
+
+RandomPick likely already exists as an npm module.
+
+There are likely many pathfinding npm packages that could replace or augment findRoute and goalOrientedRobot.
+
+
+*/
