fcd67750c19da75c5118821a686bcd55233afa27
c-rter <c@rterlawson.com>
2021-01-02 21:30:34 -0500

    solution ex 1

---
+++ 14 - Generate Table/logic.js
@@ -0,0 +1,59 @@
+<h1>Mountains</h1>
+
+<div id="mountains"></div>
+
+<script>
+  const MOUNTAINS = [
+    {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
+    {name: "Everest", height: 8848, place: "Nepal"},
+    {name: "Mount Fuji", height: 3776, place: "Japan"},
+    {name: "Vaalserberg", height: 323, place: "Netherlands"},
+    {name: "Denali", height: 6168, place: "United States"},
+    {name: "Popocatepetl", height: 5465, place: "Mexico"},
+    {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+  ];
+
+let mtnTable = (m) => {
+
+    let mData = document.createElement("table");
+  
+  	// heading section
+  
+  	for (let heading in m[0]) {
+      
+    let headerRow = document.createElement("th");
+  	headerRow.innerHTML = heading; 
+  	mData.appendChild(headerRow);
+      
+  	}
+  
+  	// end table headings
+  
+  	// table cell injection
+  
+    for (let mountain of m) {
+            	
+     	let newRow = document.createElement("tr");
+      	
+      	for (let attr in mountain) {
+	
+    		let cell = document.createElement("td");
+          	cell.innerHTML = mountain[attr];
+          	if (typeof mountain[attr] === 'number') {
+              cell.style.textAlign = "right";
+            }
+          	newRow.appendChild(cell);
+          
+        }
+      
+        mData.appendChild(newRow);
+
+    } 
+  
+  document.getElementById("mountains").appendChild(mData);
+  
+}
+
+mtnTable(MOUNTAINS);
+
+</script>
