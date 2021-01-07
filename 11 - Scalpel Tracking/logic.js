d535537d1ab10c07ba0c06161e8298eff8ee567e
c-rter <c@rterlawson.com>
2021-01-06 20:30:22 -0500

    ex1

---
+++ 11 - Scalpel Tracking/logic.js
@@ -0,0 +1,10 @@
+async function locateScalpel(nest) {
+  // Your code here.
+}
+
+function locateScalpel2(nest) {
+  // Your code here.
+}
+
+locateScalpel(bigOak).then(console.log);
+// → Butcher Shop
