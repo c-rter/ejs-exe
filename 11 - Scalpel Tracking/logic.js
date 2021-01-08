2e78a176eeaa224411dd697d43a71dfa756bf72e
Carter <38448166+c-rter@users.noreply.github.com>
2021-01-07 23:58:18 -0500

    Update logic.js

--- 11 - Scalpel Tracking/logic.js
+++ 11 - Scalpel Tracking/logic.js
@@ -1,9 +1,12 @@
 async function locateScalpel(nest) {
-  // Your code here.
+
+  
 }
 
 function locateScalpel2(nest) {
-  // Your code here.
+
+  
+  
 }
 
 locateScalpel(bigOak).then(console.log);
